2f4dd6493c57a6ed169ca144e2e9f11ee852250e
gbmatheus <gabrielmatheus2503@gmail.com>
2020-12-17 01:43:04 -0300

    Funcionalidade de remoçao de to do

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,49 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function Todo() {
-  const [nameTodo, setNameTodo] = useState("");
+
+  const [nameTodo, setNameTodo] = useState('');
+
   const [todos, setTodos] = useState([]);
+
+  // useEffect(() => {
+  //   setTodos(todos);
+  // },[todos]);
 
   function handlerAddTodo(e) {
     e.preventDefault();
 
-    const todo = { name: nameTodo, key: Date.now(), completed: false };
+    const todo = { key: Date.now(), name: nameTodo };
 
-    setTodos([...todos, todo]);
     setNameTodo('');
+    setTodos([...todos, todo]);
+  }
 
+  function handlerRemoveTodo({key}) {
+
+    const todoIndex = todos.findIndex((todo) => todo.key === key);
+
+    todos.splice(todoIndex, 1);
+
+    // Outra forma de remover
+    // const newTodos = todos.filter(todo => todo.key !== key);
+
+    setTodos([...todos]);
   }
 
   return (
     <>
       <h1>To Do App</h1>
 
+      <section>
+        <p>{Date.now()}</p>
+        <p>Task</p>
+      </section>
+
       <form onSubmit={handlerAddTodo}>
         <input
-          type="text"
+          type='text'
           value={nameTodo}
           onChange={(e) => setNameTodo(e.target.value)}
+          autoFocus={true}
+          required
         />
         <button>Add</button>
       </form>
 
-      <div>
-        <h3>To Do List</h3>
-        <div>
+      <section>
           {todos.length === 0 ? (
             <strong>Nenhum to do.</strong>
           ) : (
             <ul>
               {todos.map((todo) => (
                 <li key={todo.key}>
-                  <strong>{todo.name} </strong>
-                  <small>{ !todo.completed ? ' In progress ': ' Done '}</small>
+                  <input type="checkbox" className="checkbox" />
+
+                  <strong className={todo.completed ? "complete" : "incomplete"}>{todo.name} </strong>
+                  <button onClick={() => handlerRemoveTodo(todo)}>Remove</button>
                 </li>
               ))}
             </ul>
           )}
-        </div>
-      </div>
+        </section>
+
     </>
   );
 }
