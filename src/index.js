b3e5404c52a442ae12c29b64c07d59de34551ee1
gbmatheus <gabrielmatheus2503@gmail.com>
2020-12-17 12:50:52 -0300

    Adicionando reset  css e iniciando estlito

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './assets/styles/reset.css';
+import './assets/styles/generic.css';
+import './assets/styles/base.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
